aa06cf2e87c8fcf55ffd563da6d8b93e3281b49a
Janneth <churajanneth@gmail.com>
2020-09-09 02:45:15 -0300

    Table Finished

--- Modulo-2/Task-3/script/statistic.js
+++ Modulo-2/Task-3/script/statistic.js
@@ -10,15 +10,17 @@ let statistics = {
     avgPVrepublicans: 0,
     avgPVindependents: 0,
 
-    missedVotesDemocrats:0,
+    missedVotesDemocrats: 0,
     missedVotesRepublicans: 0,
     missedVotesIndependents: 0,
+
+    
     //array least-most Loyal
-    leastLoyal: [],
-    mostLoyal: [],
-    //array least-most Loyal
-    leastMissed: [],
-    mostMissed: []
+    leastLoyal: leastOrMost(members, "votes_with_party_pct", "least"),
+    mostLoyal: leastOrMost(members, "votes_with_party_pct", "most"),
+    // //array least-most Loyal
+    leastMissed: leastOrMost(members, "missed_votes_pct", "least"),
+    mostMissed:leastOrMost(members, "missed_votes_pct", "most")
 }
 function getPartyMembers(party) {
     return members.filter(member => member.party == party)
@@ -31,44 +33,26 @@ function avaregeVotes(party, key) {
     });
     return (sum / party.length || 0).toFixed(2);
 }
-
-//Least Or Most Loyal
+//least or most
 function leastOrMost(members, key, LorM) {
-    let tenP = Math.round(members.length / 10);//P=11 A=45
-    let mSort = [...members];
-    mSort.sort((a, b) => { return a[key] - b[key] });
-    if (LorM = "least") {
-        let leastTenM = mSort[tenP][key];
-        return statistics.leastLoyal = mSort.filter(
-            member => member[key] <= leastTenM,
-        );
-        // console.log("leastloyal");
-        // for (let i = 0; i < tenP; i++) {
-        //     console.log(statistics.leastLoyal[i].votes_with_party_pct)
-        // }
-    } else {
-        let mostTenM = mSort[mSort.length - tenP][key];
-        statistics.mostLoyal = mSort.filter(member => member[key] >= mostTenM);
-        return statistics.mostLoyal.reverse();
-        // console.log("mostloyal");
-        // for (let i = 0; i < tenP; i++) {
-        //     console.log(statistics.mostLoyal[i].votes_with_party_pct)
-        // }
-    }
-}
-statistics.leastLoyal = leastOrMost(members, "votes_with_party_pct", "least")
-statistics.mostLoyal = leastOrMost(members, "votes_with_party_pct", "most")
-statistics.leastMissed = leastOrMost(members, "missed_votes_pct", "least")
-statistics.mostMissed = leastOrMost(members, "missed_votes_pct", "most")
-statistics.avgPVdemocrats = avaregeVotes(statistics.democrats,"votes_with_party_pct")
-statistics.avgPVrepublicans = avaregeVotes(statistics.republicans,"votes_with_party_pct")
-statistics.avgPVindependents = avaregeVotes(statistics.independents,"votes_with_party_pct")
-statistics.missedVotesDemocrats = avaregeVotes(statistics.democrats, "missed_votes_pct");
-statistics.missedVotesRepublicans = avaregeVotes(statistics.republicans, "missed_votes_pct");
-statistics.missedVotesIndependents = avaregeVotes(statistics.independents, "missed_votes_pct");
+    let tenP = Math.round(members.length*10 / 100);
+    let arrayS= LorM=="most" ? [...members].sort((a,b) => a[key] - b[key]) : [...members].sort((a,b) => b[key] - a[key]);//P=11 A=45
+    let result = arrayS.slice(0,tenP);
+    let i = result.length;
+    while(i < arrayS.length && result[result.length - 1][key]== arrayS[i][key] ){
+        console.log("ingreso a WIhle")
+      result.push(arrayS[i]);
+      i++;
+      console.log("array",arrayS[i][key])
+        }
+    return result;
+      }
+
 console.log(statistics)
 
+
 let loyalty = document.getElementById("loyalty")
+
 function createGlaceTable() {
     let tbody = document.querySelector("#at_glace tbody")
     tbody.innerHTML =
@@ -97,3 +81,37 @@ function createGlaceTable() {
                     </tr>`;
 }
 createGlaceTable()
+
+
+function tableleastOrMost(ide) {
+    let table = document.getElementById(ide)
+    let tbody = document.createElement("tbody");
+    let key = loyalty ? ide+"Loyal" : ide + "Missed" ;
+    console.log(key)
+    // console.log(statistics)
+    let array=statistics[key]
+    // console.log(statistics[key])
+    // console.log(key)
+    array.forEach(array => {
+        tbody.innerHTML += `<tr>
+        <td><a class='iframe_colorbox' target='_blank' href=${array.url}>${array.first_name + ' ' + (array.middle_name || ' ') + '' + array.last_name}</a></td>
+        <td>${key? array.total_votes : array.missed_votes}</td>
+        <td>${key? array.votes_with_party_pct : array.missed_votes_pct} % </td>
+        </tr>
+        `
+      })
+  
+    table.appendChild(tbody);
+}
+tableleastOrMost("most")
+tableleastOrMost("least")
+
+
+
+statistics.avgPVdemocrats = avaregeVotes(statistics.democrats, "votes_with_party_pct")
+statistics.avgPVrepublicans = avaregeVotes(statistics.republicans, "votes_with_party_pct")
+statistics.avgPVindependents = avaregeVotes(statistics.independents, "votes_with_party_pct")
+statistics.missedVotesDemocrats = avaregeVotes(statistics.democrats, "missed_votes_pct");
+statistics.missedVotesRepublicans = avaregeVotes(statistics.republicans, "missed_votes_pct");
+statistics.missedVotesIndependents = avaregeVotes(statistics.independents, "missed_votes_pct");
+
