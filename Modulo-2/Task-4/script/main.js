7f56734118289ff6de5702706aa6f4974249d245
Janneth0 <churajanneth@gmail.com>
2020-10-02 01:49:57 -0300

    Finished

--- Modulo-2/Task-4/script/main.js
+++ Modulo-2/Task-4/script/main.js
@@ -1,20 +1,15 @@
-Vue.component("stast-table",{
-    props:['members', 'asc', 'prop1', 'prop2'],
-    
-    methods:{
-        leastOrMost: function () {
-            let tenP = Math.round(this.members.length / 10);
-            let arrayS = this.asc ? [...this.members].sort((a, b) => b[this.prop1] - a[this.prop1]):[...this.members].sort((a, b) => a[this.prop1] - b[this.prop1]);//P=11 A=45
-            let result=arrayS[tenP][this.prop1];
-            // let i = result.length;
-            // while (i < arrayS.length && result[result.length - 1][this.prop1] == arrayS[i][this.prop1]) {
-            //     console.log("Agregando repetidos que estaban fuera del Array");
-            //     result.push(arrayS[i]);
-            //     i++;
-            // }
-            return this.asc? arrayS.filter(member=>member[this.prop1]>[result]):arrayS.filter(member=>member[this.prop1]<[result]);
-        }
+Vue.component("stast-table", {
+    props: ['members', 'asc', 'prop1', 'prop2'],
 
+    methods: {
+        leastOrMost: function () {
+            let tenP = Math.round(this.members.length * 10/100);
+            console.log(tenP)
+            let arrayS = this.asc ? [...this.members].sort((a, b) => b[this.prop1] - a[this.prop1]) : [...this.members].sort((a, b) => a[this.prop1] - b[this.prop1]);//P=11 A=45
+            let result = arrayS[tenP-1][this.prop1];
+            console.log( result)
+            return this.asc ? arrayS.filter(member => member[this.prop1] >= [result]) : arrayS.filter(member => member[this.prop1] <= [result]);
+        }
     },
     template: `
                     <tbody>
@@ -54,7 +49,7 @@ let app = new Vue({
 
     },
     methods: {
-        //verificar que la api sea correcta tanto para senate como para house
+        //verificar que la api sea correcta
         getApi: function () {
             if (document.getElementById("senate")) {
                 return "https://api.propublica.org/congress/v1/113/senate/members.json"
@@ -96,15 +91,15 @@ let app = new Vue({
                 this.getstates();
                 this.statistics.democrats = this.getPartyMembers("D"),
                 this.statistics.republicans = this.getPartyMembers("R"),
-                    this.statistics.independents = this.getPartyMembers("ID")
+                this.statistics.independents = this.getPartyMembers("ID")
                 this.statistics.total = this.members
-               
+
             })
             .catch(function (error) {
                 console.log("error al traer info. ERROR:" + error)
             })
     },
-    //filtra members segun states y party
+    //filtrar members segun states y party
     computed: {
         memberFiltered: function () {
             return this.members.filter(member => this.checkedParty.includes(member.party) && (member.state == this.allselectedState || this.allselectedState == "All"));
